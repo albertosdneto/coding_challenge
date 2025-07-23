58eb0caa6b9e9fbeca928d5e8a9be7c2f50b95d7
Alberto Senna Dias Neto <albertosdneto@gmail.com>
2025-07-23 16:20:38 -0300

    setup test repository and instructions

---
+++ README.md
@@ -0,0 +1,15 @@
+# Coding Challenge
+
+## Setup environment
+```shell
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Run the test
+```shell
+pytest result.py
+```
+
+You should see a message saying ***14 passed***:
---
+++ result.py
@@ -0,0 +1,67 @@
+import pytest
+
+
+def sort(width, height, length, mass):
+    """
+    Sorts objects based on their dimensions and mass.
+
+    This function takes the dimensions and mass of objects as input and
+    sorts them accordingly. The sorting criterion or logic can be
+    defined within this function.
+
+    Args:
+        width (float): Width of the object.
+        height (float): Height of the object.
+        length (float): Length of the object.
+        mass (float): Mass of the object.
+    Returns:
+        (str): A string indicating the sorted category.
+    """
+    for item in (width, height, length, mass):
+        if not isinstance(item, (int, float)):
+            return "INVALID"
+        if item <= 0:
+            return "INVALID"
+
+    MAX_VOLUME = 1000000
+    MAX_SIZE = 150
+    MAX_MASS = 20
+
+    heavy = mass >= MAX_MASS
+    bulky = False
+
+    big_dimension = any(measure >= MAX_SIZE for measure in (width, height, length))
+    volume = width * height * length
+    if big_dimension or volume >= MAX_VOLUME:
+        bulky = True
+
+    if bulky and heavy:
+        return "REJECTED"
+
+    if bulky or heavy:
+        return "SPECIAL"
+
+    return "STANDARD"
+
+
+@pytest.mark.parametrize('width, height, length, mass, expected', [
+    (10, 10, 10, 10, "STANDARD"),
+    (10, 1, 1, 1, "STANDARD"),
+    (10, 10, 10, 20, "SPECIAL"),
+    (1, 1, 1000000, 20, "REJECTED"),
+    (1, 1, 1000000, 21, "REJECTED"),
+    (1, 1, 1000000, 10, "SPECIAL"),
+    (0, 1, 1000000, 10, "INVALID"),
+    (1, 0, 1000000, 10, "INVALID"),
+    (1, 1, 0, 10, "INVALID"),
+    (1, 1, 1000000, 0, "INVALID"),
+    ('a', 1, 1000000, 0, "INVALID"),
+    (1, 'a', 1000000, 0, "INVALID"),
+    (1, 1, 'a', 0, "INVALID"),
+    (1, 1, 1000000, 'a', "INVALID"),
+])
+def test_sort(width, height, length, mass, expected):
+    """
+    Test the sort function.
+    """
+    assert sort(width, height, length, mass) == expected
