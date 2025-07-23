6d2ec7cd101ff570d3e7c4cd6badd229789aa80d
Alberto Senna Dias Neto <albertosdneto@gmail.com>
2025-07-23 16:37:11 -0300

    fix typo

--- result.py
+++ result.py
@@ -19,9 +19,9 @@ def sort(width, height, length, mass):
     """
     for item in (width, height, length, mass):
         if not isinstance(item, (int, float)):
-            return "INVALID"
+            return "REJECTED"
         if item <= 0:
-            return "INVALID"
+            return "REJECTED"
 
     MAX_VOLUME = 1000000
     MAX_SIZE = 150
@@ -51,14 +51,14 @@ def sort(width, height, length, mass):
     (1, 1, 1000000, 20, "REJECTED"),
     (1, 1, 1000000, 21, "REJECTED"),
     (1, 1, 1000000, 10, "SPECIAL"),
-    (0, 1, 1000000, 10, "INVALID"),
-    (1, 0, 1000000, 10, "INVALID"),
-    (1, 1, 0, 10, "INVALID"),
-    (1, 1, 1000000, 0, "INVALID"),
-    ('a', 1, 1000000, 0, "INVALID"),
-    (1, 'a', 1000000, 0, "INVALID"),
-    (1, 1, 'a', 0, "INVALID"),
-    (1, 1, 1000000, 'a', "INVALID"),
+    (0, 1, 1000000, 10, "REJECTED"),
+    (1, 0, 1000000, 10, "REJECTED"),
+    (1, 1, 0, 10, "REJECTED"),
+    (1, 1, 1000000, 0, "REJECTED"),
+    ('a', 1, 1000000, 0, "REJECTED"),
+    (1, 'a', 1000000, 0, "REJECTED"),
+    (1, 1, 'a', 0, "REJECTED"),
+    (1, 1, 1000000, 'a', "REJECTED"),
 ])
 def test_sort(width, height, length, mass, expected):
     """
